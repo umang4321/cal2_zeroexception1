a54c396453f47fa5aecbf2d9a52d9cf5651cd7a7
umang4321 <ug5@njit.edu>
2021-11-23 17:20:32 -0500

    Updating the code

--- calc/addition.py
+++ calc/addition.py
@@ -1,7 +1,7 @@
 """Addition Logic"""
 from calc.calculation import Calculation
 
-#This is how you extend the Addition class with the Calculation
+
 class Addition(Calculation):
     """The addition class has one method to get the result of the the calculation A and B come from
     the calculation parent class"""

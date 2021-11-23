cbbe3276496bd177a4be84c4cf204eb8a1d74526
umang4321 <ug5@njit.edu>
2021-11-23 17:28:37 -0500

    Updating the code

--- calc/Calculation.py
+++ calc/Calculation.py
@@ -1,15 +1,13 @@
 """Logic for calculator"""
 
-
 class Calculation:
     # pylint: disable=too-few-public-methods
     """Calculation class"""
     def __init__(self, value_a, value_b):
         self.value_a = value_a
         self.value_b = value_b
-    # Class Factory Method <- bound to the class and not the instance of the class
 
     @classmethod
     def create(cls, value_a, value_b):
-        """Creating calculation"""
+        """Creating Calculation"""
         return cls(value_a, value_b)
